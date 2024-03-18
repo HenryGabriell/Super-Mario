f6ebc8f44c2a903afd3a3196e7e15c376163eaf5
Henry Gabriel <96191361+HenryGabriell@users.noreply.github.com>
2024-03-18 19:56:43 -0300

    problema do primeiro click arrumado

--- script.js
+++ script.js
@@ -1,45 +1,56 @@
-const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
-const button = document.querySelector('.button');
 
-const jump = () => {
-    mario.classList.add('jump');
 
-    setTimeout(() =>{
-        mario.classList.remove('jump');
-    }, 500);
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const mario = document.querySelector('.mario');
+    const pipe = document.querySelector('.pipe');
+    const button = document.querySelector('.button');
 
-const loop = setInterval(() => {
+    const jump = () => {
+        mario.classList.add('jump');
 
-    console.log('loop')
-
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-    
-    console.log(marioPosition);
-
-    if(pipePosition <= 120 && pipePosition >0 && marioPosition <80){
-        pipe.style.animation = 'none';
-        pipe.style.left= `${pipePosition}px`;
-
-        mario.style.animation = 'none';
-        mario.style.bottom= `${marioPosition}px`;
-
-        mario.src= './imagens/game-over.png';
-        mario.style.width= '75px'
-        mario.style.marginLeft= '50px'
-
-        button.style.display = 'block';
-        button.addEventListener('click', botaoRestart);
-
-        clearInterval(loop);
+        setTimeout(() =>{
+            mario.classList.remove('jump');
+        }, 500);
     }
 
-}, 10)
+    const loop = setInterval(() => {
 
-const botaoRestart = () => {
-    document.location.reload(true);
-}
+        console.log('loop')
 
-document.addEventListener('keydown', jump);
+        const pipePosition = pipe.offsetLeft;
+        const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+        console.log(marioPosition);
+
+        if(pipePosition <= 120 && pipePosition >0 && marioPosition <80){
+            pipe.style.animation = 'none';
+            pipe.style.left= `${pipePosition}px`;
+
+            mario.style.animation = 'none';
+            mario.style.bottom= `${marioPosition}px`;
+
+            mario.src= './imagens/game-over.png';
+            mario.style.width= '75px'
+            mario.style.marginLeft= '50px'
+
+            button.style.display = 'block';
+            button.addEventListener('click', botaoRestart);
+
+            clearInterval(loop);
+        }
+
+    }, 10)
+
+    const botaoRestart = () => {
+        document.location.reload(true);
+    }
+
+    document.addEventListener('keydown', function(event) {
+        if (event.code === 'Space') {
+            jump();
+        }
+    });
+
+    // Fazer o Mario pular quando a página carregar
+    jump();
+});
